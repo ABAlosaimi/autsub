2fccef717e7c2eb9a95a2dcb66c8f5486a4327cb
ABAlosaimi <afalih.alosaimi@gmail.com>
2025-01-30 06:49:08 +0300

    autsub

--- src/main/java/com/autsub/autsub/Company/Auth/JWTService.java
+++ src/main/java/com/autsub/autsub/Company/Auth/JWTService.java
@@ -69,7 +69,7 @@ public class JWTService {
     }
 
     private boolean isTokenExpired(String token) {
-        return extractExpiration(token).after(new Date());
+        return extractExpiration(token).before(new Date());
     }
 
     private Date extractExpiration(String token) {
